e826a2ad505c1ff2e8bd69d687bc8171856b291e
Alisher <alisher7@me.com>
2023-10-19 20:40:57 +0600

    new

--- Source/SwiftLintCore/Models/ViolationSeverity.swift
+++ Source/SwiftLintCore/Models/ViolationSeverity.swift
@@ -1,6 +1,13 @@
 /// The magnitude of a `StyleViolation`.
 
 public enum ViolationSeverity: String, Comparable, Codable, InlinableOptionType {
+    public init(fromAny value: Any, context ruleID: String) throws {
+        
+    }
+    
+    public func asOption() -> OptionType {
+        
+    }
     /// Non-fatal. If using SwiftLint as an Xcode build phase, Xcode will mark the build as having succeeded.
     case warning
     /// Fatal. If using SwiftLint as an Xcode build phase, Xcode will mark the build as having failed.
